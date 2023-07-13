df5f183f63e907ac6aa3084073bc4034cf0df62c
Mateusz Jarzębski <matt@teacode.io>
2023-07-13 16:30:21 +0200

    chore: clean

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,1 +1,15 @@
-export * from './hooks'
+import { Model } from './types/Model'
+import { FactoryOptions } from './hooks/type'
+import { makeGet } from './hooks/useGet'
+import { makeCreate } from './hooks/useCreate'
+import { makeDelete } from './hooks/useDelete'
+import { makeUpdate } from './hooks/useUpdate'
+
+export const hooksFactory = <TModel extends Model, TError = unknown>(options: FactoryOptions<TModel>) => {
+  return {
+    useGet: makeGet<TModel, TError>(options),
+    useCreate: makeCreate<TModel, TError>(options),
+    useDelete: makeDelete<TModel, TError>(options),
+    useUpdate: makeUpdate<TModel, TError>(options),
+  }
+}
